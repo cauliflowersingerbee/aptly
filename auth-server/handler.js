2dd74920f2a9a334aa4dc967a9d87c5912138059
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2021-12-09 22:41:50 +0100

    Update handler.js

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -10,7 +10,7 @@ const credentials = {
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  redirect_uris: ["https://cauliflowersingerbee.github.io/aptly/"],
+  redirect_uris: ["https://cauliflowersingerbee.github.io/aptly"],
   javascript_origins: ["https://cauliflowersingerbee.github.io", "http://localhost:3000"],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
