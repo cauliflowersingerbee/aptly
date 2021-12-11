e210d1406ad2a2a81e30b74a05c949ba2e12318a
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2021-12-11 22:22:50 +0100

    set up the calendar

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -6,13 +6,14 @@ const credentials = {
   client_id: process.env.CLIENT_ID,
   project_id: process.env.PROJECT_ID,
   client_secret: process.env.CLIENT_SECRET,
+  calendar_id: process.env.CALENDAR_ID,
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
   redirect_uris: ["https://cauliflowersingerbee.github.io/aptly"],
   javascript_origins: ["https://cauliflowersingerbee.github.io", "http://localhost:3000"],
 };
-const { client_secret, client_id, redirect_uris} = credentials;
+const { client_secret, client_id, redirect_uris, calendar_id} = credentials;
 const oAuth2Client = new google.auth.OAuth2(
   client_id,
   client_secret,
