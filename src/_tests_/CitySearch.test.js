43b782062bc037043284e651d4d65bc85b981124
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2021-12-13 09:53:50 +0100

    Update CitySearch.test.js

--- src/_tests_/CitySearch.test.js
+++ src/_tests_/CitySearch.test.js
@@ -13,3 +13,9 @@ test('renders a list of suggestions', () => {
     const CitySearchWrapper = shallow(<CitySearch />);
     expect(CitySearchWrapper.find('.suggestions')).toHaveLength(1);
   });
+
+  test('renders text input correctly', () => {
+    const CitySearchWrapper = shallow(<CitySearch />);
+    const query = CitySearchWrapper.state('query');
+    expect(CitySearchWrapper.find('.city').prop('value')).toBe(query);
+  });
