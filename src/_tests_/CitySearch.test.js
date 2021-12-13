fd52b73e0f04ee0d19eaf3ca6c89f44817f8ad0a
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2021-12-13 12:20:37 +0100

    updated tests

--- src/_tests_/CitySearch.test.js
+++ src/_tests_/CitySearch.test.js
@@ -42,4 +42,17 @@ test('renders text input correctly', () => {
       expect(CitySearchWrapper.find('.suggestions li').at(i).text()).toBe(suggestions[i]);
     }
   });
+
+  test('suggestion list match the query when changed', () => {
+    CitySearchWrapper.setState({ query: '', suggestions: [] });
+    CitySearchWrapper.find(".city").simulate("change", {
+      target: { value: "Berlin" },
+    });
+    const query = CitySearchWrapper.state("query");
+    const filteredLocations = locations.filter((location) => {
+      return location.toUpperCase().indexOf(query.toUpperCase()) > -1;
+    });
+    expect(CitySearchWrapper.state("suggestions")).toEqual(filteredLocations);
+  });
 });
+
