30ed3c516d6e3a2739a33bd6d271e1ce7799c4be
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2021-12-14 09:56:26 +0100

    updated event.test.js

--- src/Event.js
+++ src/Event.js
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 
 class Event extends Component {
   render() {
-    return <div></div>;
+    return <div className="summary">
+
+    </div>;
   }
 }
 export default Event;
--- src/_tests_/Event.test.js
+++ src/_tests_/Event.test.js
@@ -4,9 +4,41 @@ import Event from '../Event';
 import { mockData } from '../mock-data';
 
 describe('<Event /> component', () => {
+    let EventWrapper;
+    beforeAll(() => {
+      EventWrapper = shallow(<Event event={mockData[0]} />);
+    });
+    //tests for basic information verifying if all necessary
+    //elements have been rendered
+    
 
-  test('render collapsed details by default', () => {
-    const EventListWrapper = shallow(<EventList events={mockData} />);
-    expect(EventListWrapper.find(Event)).toHaveLength(mockData.length);
-  });
-});
+
+    //1. event element collapsed by default
+    test('render collapsed event by default', () => {
+        expect(EventWrapper.find('.summary')).toHaveLength(1);
+      });
+    });
+    
+
+
+
+
+//2. It must be able to show/hide an event's details.
+
+//Scenario 1: An event element is collapsed by default.
+
+//Given a user is on the main page
+//When they have not clicked on an event
+//Then the event element should be collapsed by default
+
+//Scenario 2: User can expand an event to see its details
+
+//Given a user is interested in more details
+//When they click on an event
+//Then they can expand it to get more information about the event that interests them
+
+//Scenario 3: User can collapse an event to hide its details
+
+//Given a user has already seen the relevant details
+//When they click on an event
+//Then they can collapse it in order to get less details displayed to them//
