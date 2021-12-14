554766d9e1ad41052b816c50456b7bad1153411f
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2021-12-14 10:43:02 +0100

    added summary test

--- src/Event.js
+++ src/Event.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
-import { Button } from 'react-bootstrap'; 
+import { Button } from "react-bootstrap"; 
 
 class Event extends Component {
   
 
   render() {
-    return <div>
+    return <div className="event">
+      <h2 className="summary">event summary</h2>
+
       <Button variant="primary" className="show-more" >
                     Toggle details
       </Button>
--- src/_tests_/Event.test.js
+++ src/_tests_/Event.test.js
@@ -8,10 +8,51 @@ describe('<Event /> component', () => {
     beforeAll(() => {
       EventWrapper = shallow(<Event event={mockData[0]} />);
     });
+
     //tests for basic information verifying if all necessary
     //elements have been rendereds
+
     //1.We need a dropdown button
     test("show more button is displayed", () => {
         expect(EventWrapper.find(".show-more")).toHaveLength(1);
       });
+   
+    
+    //2.We need to show event details in dropdown button ie 
+    //  summary, location, date & timezone
+    test("summary is displayed", () => {
+        expect(EventWrapper.find(".summary")).toHaveLength(1);
+      });
+
     });
+
+//Scenario 1: An event element is collapsed by default.
+    //test('render collapsed event by default', () => {
+      //  expect(EventWrapper.state('.collapsed')).toBe(true);
+      //});
+    //});
+    
+
+
+
+
+//2. It must be able to show/hide an event's details.
+
+//Scenario 1: An event element is collapsed by default.
+
+//Given a user is on the main page
+//When they have not clicked on an event
+//Then the event element should be collapsed by default
+
+//Scenario 2: User can expand an event to see its details
+
+//Given a user is interested in more details
+//When they click on an event
+//Then they can expand it to get more information about the event that interests them
+
+//Scenario 3: User can collapse an event to hide its details
+
+//Given a user has already seen the relevant details
+//When they click on an event
+//Then they can collapse it in order to get less details displayed to them//
+
