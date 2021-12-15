a17bac97b95d711938b9509c239ac15d9029ac3a
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2021-12-15 11:35:19 +0100

    created new function in App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,22 @@ import EventList from './EventList';
 import CitySearch from './CitySearch';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NumberOfEvents from './NumberOfEvents';
+import { getEvents } from './api';
 
 
 class App extends Component {
   state = {
     events: [], 
     locations: []
+  }
+
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents = events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents
+      });
+    });
   }
 
   render() {
