a38674e80328cea68b8ff3559749c5f483bafb55
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2021-12-15 13:06:52 +0100

    updated files

--- src/api.js
+++ src/api.js
@@ -9,5 +9,39 @@ export const extractLocations = (events) => {
   };
 
 export const getEvents = async () => {
-  return mockData;
+  if (window.location.href.startsWith('http://localhost')) {
+   return mockData;
+  }
 };
+
+export const getAccessToken = async () => {
+  const accessToken = localStorage.getItem('access_token');
+  const tokenCheck = accessToken && (await checkToken(accessToken));
+
+  if (!accessToken || tokenCheck.error) {
+    await localStorage.removeItem("access_token");
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get("code");
+    if (!code) {
+      const results = await axios.get(
+        "https://eeah9wx4kb.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
+      );
+      const { authUrl } = results.data;
+      return (window.location.href = authUrl);
+    }
+    return code && getToken(code);
+  }
+  return accessToken;
+}
+
+const checkToken = async (accessToken) => {
+  const result = await fetch(
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+  )
+    .then((res) => res.json())
+    .catch((error) => error.json());
+
+  return result;
+};
+
+
