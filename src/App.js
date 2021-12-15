adc0c946521bb2abcfd50d48ada8946191e6583f
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2021-12-15 12:29:55 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ import EventList from './EventList';
 import CitySearch from './CitySearch';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NumberOfEvents from './NumberOfEvents';
-import { getEvents } from './api';
+import { getEvents, extractLocations } from './api';
+
 
 
 class App extends Component {
   state = {
     events: [], 
     locations: []
+  }
+
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) });
+      }
+    });
+  }
+
+  componentWillUnmount(){
+    this.mounted = false;
   }
 
   updateEvents = (location) => {
