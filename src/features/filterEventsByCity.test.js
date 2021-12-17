43c199eaecddef1b2e23940116a03f958bda596c
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2021-12-17 22:57:14 +0100

    Update filterEventsByCity.test.js

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -3,14 +3,15 @@ import { loadFeature, defineFeature } from 'jest-cucumber';
 const feature = loadFeature('./src/features/filterEventsByCity.feature');
 
 defineFeature(feature, test => {
-    
+
     test('When user hasn’t searched for a city, show upcoming events from all cities.', ({ given, when, then }) => {
         given('user hasn’t searched for any city', () => {
-
+            
         });
 
+        let AppWrapper;
         when('the user opens the app', () => {
-
+            AppWrapper = mount(<App />);
         });
 
         then('the user should see the list of upcoming events.', () => {
