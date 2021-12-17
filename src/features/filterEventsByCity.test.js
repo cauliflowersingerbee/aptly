ef544949b9298def2fbf1c1defc9d9d1f111e768
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2021-12-17 23:03:26 +0100

    Update filterEventsByCity.test.js

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -20,7 +20,8 @@ defineFeature(feature, test => {
         });
 
         then('the user should see the list of upcoming events.', () => {
-
+            AppWrapper.update();
+            expect(AppWrapper.find('.event')).toHaveLength(mockData.length);
         });
     });
 
