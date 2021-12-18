6c8df8307807a6f928321355dce0efb19e86af86
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2021-12-18 21:40:57 +0100

    created third feature test

--- src/features/showHideEvents.feature
+++ src/features/showHideEvents.feature
@@ -1,5 +1,16 @@
 Feature: Show/hide an event details
 
 Scenario: An event element is collapsed by default.
-Scenario: User can expand an event to see its details.
-Scenario: User can collapse an event to hide its details.
+Given a user is on the main page
+When they have not clicked on an event
+Then the event element should be collapsed by default
+
+Scenario: User can expand an event to see its details
+Given a user is interested in more details
+When they click on an event
+Then they can expand it to get more information about the event that interests them
+
+Scenario: User can collapse an event to hide its details
+Given a user has already seen the relevant details
+When they click on an event
+Then they can collapse it in order to get less details displayed to them
--- src/features/showHideEvents.test.js
+++ src/features/showHideEvents.test.js
@@ -4,4 +4,45 @@ const feature = loadFeature('./src/features/showHideEvents.feature');
 
 defineFeature(feature, test => {
 
+    test('An event element is collapsed by default.', ({ given, when, then }) => {
+        given('a user is on the main page', () => {
+
+        });
+
+        when('they have not clicked on an event', () => {
+
+        });
+
+        then('the event element should be collapsed by default', () => {
+
+        });
+    });
+
+    test('User can expand an event to see its details', ({ given, when, then }) => {
+        given('a user is interested in more details', () => {
+
+        });
+
+        when('they click on an event', () => {
+
+        });
+
+        then('they can expand it to get more information about the event that interests them', () => {
+
+        });
+    });
+
+    test('User can collapse an event to hide its details', ({ given, when, then }) => {
+        given('a user has already seen the relevant details', () => {
+
+        });
+
+        when('they click on an event', () => {
+
+        });
+
+        then('they can collapse it in order to get less details displayed to them', () => {
+
+        });
+    });
 });
