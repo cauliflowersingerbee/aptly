208c3bf5f1ba8a13955add69edbe22126d9dac3e
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2021-12-18 21:53:53 +0100

    updated feature tests

--- src/features/specifyNumberOfEvents.feature
+++ src/features/specifyNumberOfEvents.feature
@@ -1,3 +1,11 @@
-Feature 3: Specify Number of Events
-Scenario 1: When user hasn’t specified a number, 20 is the default number.
-Scenario 2: User can change the number of events they want to see.
+Feature: Specify Number of Events
+
+Scenario: When user hasn’t specified a number, 20 is the default number.
+Given a user has not specified a preferred number 
+When they view the main page
+Then 20 is the number that is displayed to them
+
+Scenario: User can change the number of events they want to see.
+Given a user would like to change the number of events
+When they click the dropdown
+Then they can specify the number of events they would like to see"
--- src/features/specifyNumberOfEvents.test.js
+++ src/features/specifyNumberOfEvents.test.js
@@ -3,5 +3,32 @@ import { loadFeature, defineFeature } from 'jest-cucumber';
 const feature = loadFeature('./src/features/specifyNumberOfEvents.feature');
 
 defineFeature(feature, test => {
+    test('When user hasn’t specified a number, 20 is the default number.', ({ given, when, then }) => {
+        given('a user has not specified a preferred number', () => {
+
+        });
+
+        when('they view the main page', () => {
+
+        });
+
+        then(/^(\d+) is the number that is displayed to them$/, (arg0) => {
+
+        });
+    });
+    
+    test('User can change the number of events they want to see.', ({ given, when, then }) => {
+        given('a user would like to change the number of events', () => {
+
+        });
+
+        when('they click the dropdown', () => {
+
+        });
+
+        then('they can specify the number of events they would like to see"', () => {
+        
+        });
+    });
 
 });
