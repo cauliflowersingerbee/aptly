e8f9f077b4d14ce74c71ec45ba18bd1a761e99fd
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2021-12-20 16:35:46 +0100

    updated handleInputChanged to add info alert

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -13,11 +13,19 @@ class CitySearch extends Component {
     const suggestions = this.props.locations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
-    this.setState({
+    if (suggestions.length === 0) {
+      this.setState({
+        query: value,
+        infoText: 'We cannot find the city you are looking for. Please try another city',
+      });
+    } else {
+      return this.setState({
       query: value,
       suggestions,
+      infoText:''
     });
   };
+};
 
   handleItemClicked = (suggestion) => {
     this.setState({
