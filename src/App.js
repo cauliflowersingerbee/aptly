3c627c949e95c65a4883b5a052d6a5aa4b72ae3f
cauliflowersingerbee <86426968+cauliflowersingerbee@users.noreply.github.com>
2021-12-22 22:19:25 +0100

    updated scatterchart customization to be responsive

--- src/App.js
+++ src/App.js
@@ -110,15 +110,16 @@ className="App" />
         </Row>
         <Row>
          <h4>Events in each city:</h4>
-
-         <ScatterChart width={730} height={250}
-              margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
+         
+         <ResponsiveContainer height={400} >
+         <ScatterChart margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
               <CartesianGrid strokeDasharray="3 3" />
               <XAxis type="category" dataKey="city" name="city" />
               <YAxis type="number"  dataKey="number" name="number of events" allowDecimals={false}/>
               <Tooltip cursor={{ strokeDasharray: '3 3' }} />
               <Scatter data={this.getData()} fill="#8884d8" />
          </ScatterChart>
+         </ResponsiveContainer>
          
          </Row>
          <Row>
